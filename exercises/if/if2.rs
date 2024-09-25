6f3d7e9a42640174664f4d4ad8b9a5e17843ee71
9NH3 <WangJiuAnnn@outlook.com>
2024-09-25 20:24:27 +0800

    finish 3

--- exercises/if/if2.rs
+++ exercises/if/if2.rs
@@ -5,13 +5,14 @@
 //
 // Execute `rustlings hint if2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+// I AM NOT DON
 
 pub fn foo_if_fizz(fizzish: &str) -> &str {
     if fizzish == "fizz" {
         "foo"
-    } else {
-        1
+    } 
+    else{
+        "baz"
     }
 }
 
@@ -27,7 +28,7 @@ mod tests {
 
     #[test]
     fn bar_for_fuzz() {
-        assert_eq!(foo_if_fizz("fuzz"), "bar")
+        assert_eq!(foo_if_fizz("fuzz"), "baz")
     }
 
     #[test]
--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -16,8 +16,13 @@
 // I AM NOT DONE
 
 // Put your function here!
-// fn calculate_price_of_apples {
-
+fn calculate_price_of_apples(let quantity)->i32 {
+    if quantity<=40{
+        2*quantity
+    }else{
+        quantity
+    }
+}
 // Don't modify this function!
 #[test]
 fn verify_test() {
