961e59290e108d36ad98f2cef7768eaa0cc3b9f4
9NH3 <WangJiuAnnn@outlook.com>
2024-10-18 11:16:56 +0800

    14

--- exercises/lifetimes/lifetimes3.rs
+++ exercises/lifetimes/lifetimes3.rs
@@ -5,13 +5,12 @@
 // Execute `rustlings hint lifetimes3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+// I AM NOT DON
 
-struct Book {
-    author: &str,
-    title: &str,
+struct Book<'a> {
+    author: &'a str,
+    title: &'a str,
 }
-
 fn main() {
     let name = String::from("Jill Smith");
     let title = String::from("Fish Flying");
